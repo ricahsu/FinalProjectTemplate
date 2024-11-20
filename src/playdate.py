701652f9f700d532f671106e0eb02f610e0b4b6c
Rica Schultz <ricahsu0507@gmail.com>
2024-11-19 21:49:31 -0700

    Mock data

---
+++ src/playdate.py
@@ -0,0 +1,20 @@
+def get_options():
+
+
+def get_parks(dis:int) -> str: 
+
+
+
+
+
+
+
+
+
+
+def main():
+    print("Welcome to dog park playdate. ")
+    get_options()
+
+if __name__ == "__main__":
+    main()
